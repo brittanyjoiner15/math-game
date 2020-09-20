e1e7320d5bf45f2a41e4388ee355248f8a8ef87f
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-09-20 09:35:07 -0500

    added basic functionality

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 //make a function that generates the equation
-var generateEquation = function () {
-  var x = parseInt(Math.random() * 100);
-  var y = parseInt(Math.random() * 100);
-  var z = x + y;
-  console.log(x, y, z);
-};
 
-console.log(generateEquation());
+$(document).ready(function () {
+  var currentEquation;
 
-//make a function that checks the input and see if it matches z when submit is clicked
+  var generateEquation = function () {
+    var question = {};
+    var x = parseInt(Math.random() * 100);
+    var y = parseInt(Math.random() * 100);
+    question.answer = x + y;
+    question.equation = String(x) + " + " + String(y);
 
-$("#check_guess").submit(function (event) {
-  event.preventDefault();
-  var guess = $("#guess").val();
-  console.log(guess);
-  //   if ((guess = z)) {
-  //     console.log("right");
-  //   } else {
-  //     console.log("wrong");
-  //   }
+    return question;
+  };
+
+  currentEquation = generateEquation();
+  $("#equation").text(currentEquation.equation);
+
+  var score = 0;
+  $("#score").text("Score = " + score);
+
+  $("#check_guess").on("submit", function () {
+    event.preventDefault();
+    var guess = parseInt($("#guess").val());
+    console.log(guess);
+    console.log(currentEquation.answer);
+    if (guess === currentEquation.answer) {
+      console.log("right!");
+      $("#status").text("correct!");
+      score = score + 1;
+      $("#score").text("Score = " + score);
+      currentEquation = generateEquation();
+      $("#equation").text(currentEquation.equation);
+    } else {
+      $("#status").text("wrong!");
+    }
+  });
 });
 
-//set the score
+//make a function that checks the input and see if it matches question.answer when submit is clicked ; done
+
+//set the score: done
 
 //make a function that starts a timer when start button is clicked
 
 //update the checkInput function to add a second to the timer and update the score if answer is correct
+
+//when timer runs out, say "game end" and change color of start button to primary and make it start the timer and reset the score
